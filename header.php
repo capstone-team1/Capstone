50021b83027fb0f3463956d1550e4728859f6511
leoraelkins <ldsliney@email.neit.edu>
2015-08-22 19:00:51 -0400

    I added the header and footer php files but only worked on the header so far but of course can't get the stupid thing to show. Can't even get the new favicon to show up. No clue what's going on but after wasting a few hours on this I'm going to start on our other behind the scenes stuff instead for now.  Feel free to let me know if you can figure out what I'm doing wrong. Also added an S to the end of the BLOG title. Seemed more appropriate right? Oh and this commit should include the new files if i'm doing it right!

---
+++ header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<div id="navbar-full">
+    <div id="navbar">
+    <!-- navbar-default can be changed with navbar-ct-blue navbar-ct-azzure navbar-ct-red navbar-ct-green navbar-ct-orange -->
+        <nav class="navbar navbar-ct-red navbar-transparent navbar-fixed-top" role="navigation">
+          <div class="alert alert-success hidden">
+            <div class="container">
+                <b>Lorem ipsum</b> dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            </div>
+          </div>
+
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Tattoos By Amanda</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Tattoos</a></li>
+                        <li><a href="#">Drawings</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                      </ul>
+                </li>
+                  <li><a href="#about-jump">About</a></li>
+                  <li><a href="#contact-jump">Contact</a></li>
+                  <li><a href="#">Links</a></li>
+                  <li><a href="#blog-jump">Blogs</a></li>
+                <li>
+                    <a href="javascript:void(0);" data-toggle="search" class="hidden-xs"><i class="fa fa-search"></i></a>
+                </li>
+              </ul>
+               <form class="navbar-form navbar-left navbar-search-form" role="search">
+                 <div class="form-group">
+                      <input type="text" value="" class="form-control" placeholder="Search...">
+                 </div>
+              </form>
+              <ul class="nav navbar-nav navbar-right">
+                    <!--insert social media here-->
+                  <div class="form-group">
+
+                      <a href="#"><i class="fa fa-instagram fa-2x"></i></a>
+                      <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+                      <a href="#"><i class="fa fa-envelope-o fa-2x"></i></a>
+                  </div>
+               </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+</head>
+
+<body>
+</body>
+
+</html>
