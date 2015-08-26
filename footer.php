70b17b79f6f8bbb0aa19602480774473626d66a4
000535495 <Lilyorae79>
2015-08-26 19:28:09 -0400

    Continued working on header and footer php files.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<!-- end main -->
+
+        <div class="footer">
+            <div class="overlayer">
+                <div class="container">
+                    <div class="row support">
+                        <div class="col-sm-4">
+                            <i class="fa fa-copyright"></i>
+                        </div>
+                        <div class="col-sm-4">
+                            <h3>Col 2</h3>
+                        </div>
+                        <div class="col-sm-4">
+                            <h3>Col 3</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="credits">
+                            &copy; 2015 Amanda Orcutt
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Tattoo's by Amanda</title>
+
+    <!-- favicon link -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+
+    <link href="assets/css/get-shit-done.css" rel="stylesheet" />
+    <link href="assets/css/demo.css" rel="stylesheet" />
+
+    <!--     Custom Google Fonts, "Shadows into Light", "Permanent Marker" -->
+    <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light|Permanent+Marker' rel='stylesheet' type='text/css'>
+
+    <!--     Font Awesome     -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!--Parallax JS-->
+    <script src="/Capstone/assets/js/parallax.js" type=text/javascript></script>
+
+    <!--JQuery-->
+    <script src="assets/jquery/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+
+    <!--Bootstrap and gsdk-->
+    <script src="assets/js/bootstrap.js" type="text/javascript"></script>
+    <script src="assets/js/gsdk-checkbox.js"></script>
+    <script src="assets/js/gsdk-radio.js"></script>
+    <script src="assets/js/gsdk-switch.js"></script>
+    <script src="assets/js/get-shit-done.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+</head>
+
+<body>
+
+<div id="navbar-full">
+
+    <div id="navbar">
+    <!-- navbar-default can be changed with navbar-ct-blue navbar-ct-azzure navbar-ct-red navbar-ct-green navbar-ct-orange -->
+        <nav class="navbar navbar-ct-red navbar-transparent navbar-fixed-top" role="navigation">
+          <div class="alert alert-success hidden">
+            <div class="container">
+                <b>Lorem ipsum</b> dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            </div>
+          </div>
+
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Tattoos By Amanda</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Tattoos</a></li>
+                        <li><a href="#">Drawings</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                      </ul>
+                </li>
+                  <li><a href="#about-jump">About</a></li>
+                  <li><a href="#contact-jump">Contact</a></li>
+                  <li><a href="#">Links</a></li>
+                  <li><a href="#blog-jump">Blogs</a></li>
+                <li>
+                    <a href="javascript:void(0);" data-toggle="search" class="hidden-xs"><i class="fa fa-search"></i></a>
+                </li>
+              </ul>
+                <form class="navbar-form navbar-left navbar-search-form" role="search">
+                   <div class="form-group">
+                      <input type="text" value="" class="form-control" placeholder="Search...">
+                   </div>
+                </form>
+              <ul class="nav navbar-nav navbar-right">
+                    <!--insert social media here-->
+                  <div class="form-group">
+                      <a href="#"><i class="fa fa-instagram fa-2x"></i></a>
+                      <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+                      <a href="#"><i class="fa fa-envelope-o fa-2x"></i></a>
+                  </div>
+               </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+
+        <!-- THIS IS LARGE BLURRED IMAGE UNDER NAV BAR
+            <div class="blurred-container">
+                <div class="img-src" style="background-image: url('assets/img/wallpaper1.jpg')"></div>
+            </div> -->
+    </div><!-- end navbar -->
+
+</div> <!-- end menu-dropdown -->
