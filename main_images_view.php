10348015d0c39d350b4a1d507de168a3bd85f84d
leoraelkins <ldsliney@email.neit.edu>
2015-08-29 20:45:57 -0400

    In the middle of working on user images pages... not even close to done

---
+++ main_images_view.php
@@ -0,0 +1,13 @@
+<?php include_once("main_header.php"); ?>
+
+
+<div id="thumbnails">
+
+    <button type="button" class= "btn btn-primary" id="btnThumb">FILL</button>
+
+</div>
+
+
+
+
+<?php include_once("main_footer.php"); ?>
---
+++ main_links.js
@@ -0,0 +1,10 @@
+<?php include_once("main_header.php"); ?>
+
+
+
+
+
+
+
+
+<?php include_once("main_footer.php"); ?>
