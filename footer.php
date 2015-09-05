9c507c439b88b507dc787a03ead0b07dae6df279
leoraelkins <ldsliney@email.neit.edu>
2015-09-05 19:07:31 -0400

    Updated my new branch with my files and incorporated header and footers (working fine).  I'm guessing your going to have to merge with master again. Haven't touched images code yet, just included the files.

--- footer.php
+++ footer.php
@@ -1,1 +1,21 @@
-<?php
+<!-- end main -->
+
+
+<div class="footer">
+    <!--<div class="overlayer">-->
+    <!--<div class="container">-->
+    <!--<div class="row support">-->
+    <!--<div class="col-sm-4">-->
+    <!--<i class="fa fa-copyright"></i>-->
+    <!--</div>-->
+    <!--</div>-->
+    <div class="row">
+        <div class="credits">
+            &copy; 2015 Amanda Orcutt
+        </div>
+    </div>
+    <!--</div>-->
+    <!--</div>-->
+</div>
+</body>
+</html>
